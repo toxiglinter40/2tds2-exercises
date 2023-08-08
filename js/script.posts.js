283c8a8991e47b6262bdbdf3d4aba8129c0935cf
toxiglinter40 <124887158+toxiglinter40@users.noreply.github.com>
2023-08-08 14:09:46 -0300

    creat save post function

---
+++ js/script.posts.js
@@ -0,0 +1,6 @@
+function savepost(){
+    const title = document.getElementById("title").innerHTML
+    const resume = document.getElementById("category").innerHTML
+    const author = document.getElementById("resume").innerHTML
+    const date = document.getElementById("author").innerHTML
+}
